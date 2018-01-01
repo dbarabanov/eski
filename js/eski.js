9f7b592d5a5d73168c373b9f4baeb9426bcc68a8
Dmitri Barabanov <dmitri.barabanov@gmail.com>
2018-01-01 03:34:30 +0300

    fixing collapse menu second attempt

--- js/eski.js
+++ js/eski.js
@@ -1,5 +1,3 @@
-$(document).on('click','.navbar-collapse.in',function(e) {
-    if( $(e.target).is('a') ) {
-        $(this).collapse('hide');
-    }
+$('.navbar-nav>a').on('click', function(){
+    $('.navbar-collapse').collapse('hide');
 });
